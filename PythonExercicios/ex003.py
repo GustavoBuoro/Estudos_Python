ab74b89d606405555f15846617d81abd8339980c
Pam2 <gustavobuoroo@gmail.com>
2025-08-21 17:42:59 -0300

    https://github.com/GustavoBuoro/Estudos_Python.git

---
+++ PythonExercicios/ex003.py
@@ -0,0 +1,5 @@
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+soma = n1 + n2
+
+print('A soma entre {} e {} é igual a {}'.format(n1, n2, soma))
---
+++ PythonExercicios/ex012.py
@@ -0,0 +1,5 @@
+p = float(input('Digit o preço do produto R$: '))
+
+novo = p-(p*5/100)
+
+print('O produce que custava {}, com 5% de desconto custa :{}R$'.format(p,novo))
